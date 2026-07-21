369b9cece577bbdcb06e19f4384a4c0163451859
Christophe Guillon <christophe.guillon@inria.fr>
2026-07-21 19:04:39 +0200

    Use generic image

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 echo "Executing $0 $*"
-echo "Env:"
-env
 
-set -x
 uid="${DOCKER_UID:-1000}"
 gid="${DOCKER_GID:-1000}"
 
+chown -R "$uid:$gid" "$CCACHE_DIR"
+
 [ $# -gt 0 ] || set -- bash
-exec gosu "$uid:$gid" "$@"
+
+set -x
+exec gosu "$uid:$gid" env USER=guest HOME=/home/guest "$@"
 
